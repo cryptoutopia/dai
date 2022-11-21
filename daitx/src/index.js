a2d9459cffd2a7b1eddd0d73fefd919d6fd02425
cryptoutopia <pczibor@gmail.com>
2022-11-22 00:42:56 +0100

    delete useless code

--- daitx/src/index.js
+++ daitx/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals.js';
 import Web3 from 'web3';
 import Contract from 'web3-eth-contract';
 import './index.css';
@@ -170,8 +168,3 @@ root.render(
     <DaiTxn />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
